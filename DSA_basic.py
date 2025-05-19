483e482c76e141c577dca83b5b98714f1bbea30e
Sahilkatiyar17 <sahilkatiyar2024@gmail.com>
2025-05-20 00:05:33 +0530

    Initial commit

---
+++ DSA_basic.py
@@ -0,0 +1,60 @@
+class Graph:
+  def __init__(self,num_vertices):
+    self.v = num_vertices
+    self.adj_matrix = [[0 for _ in range(self.v)] for _ in range(self.v)]
+    # self.adj_matrix = np.zeros((self.v,self.v), dtype=int)
+  def add_edge(self, u , v,w):
+    self.adj_matrix[u][v]=w
+    self.adj_matrix[v][u]=w
+
+  def remove_edge(self,u,v):
+    self.adj_matrix[u][v]=0
+    self.adj_matrix[v][u]=0
+
+  def __len__(self):
+    return self.v
+
+  def print_matrix(self):
+    for row in self.adj_matrix:
+      print(row)
+
+g = Graph(5)
+g.add_edge(0,1,34)
+g.add_edge(0,4,22)
+g.add_edge(1,2,9)
+
+g.print_matrix()
+
+
+
+#adj_list representation of graph 
+class Graph:
+  def __init__(self):
+    self.adj_list={}
+
+  def add_vertex(self,v):
+    if v not in self.adj_list:
+      self.adj_list[v]=[]
+
+  def add_edge(self,u,v):
+    self.add_vertex(u)
+    self.add_vertex(v)
+    self.adj_list[u].append(v)
+    self.adj_list[v].append(u)
+
+  def remove_edge(self,u,v):
+    if v in self.adj_list[u]:
+      self.adj_list[u].remove(v)
+    if u in self.adj_list[v]:
+      self.adj_list[v].remove(u)
+
+  def print_graph(self):
+    for vertex in self.adj_list:
+      print(vertex,":",self.adj_list[vertex])
+
+g = Graph()
+g.add_edge(0,1)
+g.add_edge(0,4)
+g.add_edge(1,2)
+
+g.print_graph()
